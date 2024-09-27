f730e648798dc8ffbd57722f90e859c58e6b4830
vivekkeyvalue <vivek@keyvalue.systems>
2024-09-28 04:55:16 +0530

    feat: minor fixes

--- src/components/follow-up/FollowUp.js
+++ src/components/follow-up/FollowUp.js
@@ -1,12 +1,12 @@
 const FollowUp = ({ questions, onClick }) => {
   return (
-    <div className="flex justify-center items-center gap-3">
+    <div className="flex justify-center items-center gap-3 mt-6">
       {questions?.map((followUp, index) => (
         <div
           role="presentation"
           onClick={() => onClick(followUp)}
           key={index}
-          className="cursor-pointer bg-[#DCD3E9] py-1 px-[20px] rounded-[30px] flex items-center border border-border mb-2 w-fit text-[14px] font-bold"
+          className="cursor-pointer bg-[#DCD3E9] py-2 px-[30px] rounded-[30px] flex items-center border-[2px] border-black mb-2 w-fit text-[14px] font-bold"
         >
           {followUp}
         </div>
