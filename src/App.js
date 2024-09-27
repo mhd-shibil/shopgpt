85932028000f632365904dd3f45351c30a4151b7
vivekkeyvalue <vivek@keyvalue.systems>
2024-09-27 15:28:43 +0530

    feat: blur the tile if chatbox is opem

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
+import { useState } from "react";
+
 import TopBar from "./components/top-bar/TopBar";
 import TileList from "./components/tile/TileList";
+import { ChatBox } from "./components/ChatBox";
 
 import "./App.css";
-import './App.css';
-import { ChatBox } from './components/ChatBox';
 
 function App() {
+  const [isFocused, setIsFocused] = useState(false);
   return (
     <div className="App">
       <TopBar />
-      <div className="h-[calc(100vh-200px)] mt-[200px] overflow-auto px-[32px] no-scrollbar pb-16">
+      <div
+        className={`h-[calc(100vh-200px)] mt-[200px] overflow-auto px-[32px] no-scrollbar pb-16 ${
+          isFocused ? "opacity-50 blur-sm" : ""
+        }`}
+      >
         <TileList />
       </div>
-        <ChatBox/>
+      <ChatBox isFocused={isFocused} setIsFocused={setIsFocused} />
     </div>
   );
 }
