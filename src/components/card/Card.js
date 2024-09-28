1cf70f9cff8f01b8f9b3e02b8bd501f55e5d3d33
vivekkeyvalue <vivek@keyvalue.systems>
2024-09-28 06:18:30 +0530

    fix: minor changes

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,21 +2,12 @@ import React from "react";
 import ShopIcon from "./ShopIcon";
 
 const Card = ({ details, handleTry }) => {
-  // todo: update site image and site name
-  const {
-    title,
-    price,
-    imgUrl,
-    url,
-    discount = "",
-    isTryOn = true,
-    store,
-  } = details;
+  const { title, price, imgUrl, url, discount = "", type, store } = details;
 
   return (
     <div
       className={`card bg-[#F9F6FB] rounded-lg border-[1px] shadow-md border-black flex flex-col justify-between items-center pb-4 ${
-        isTryOn ? "h-full min-h-[340px]" : "h-full min-h-[300px]"
+        type === "Fashion" ? "h-full min-h-[340px]" : "h-full min-h-[300px]"
       }`}
       style={styles.card}
       role="presentation"
@@ -38,7 +29,7 @@ const Card = ({ details, handleTry }) => {
 
       <div className="w-full items-center justify-center">
         <ShopIcon shopLink={url} style={styles.siteLink} shopName={store} />
-        {isTryOn && (
+        {type === "Fashion" && (
           <button
             onClick={() => handleTry(imgUrl)}
             className="mt-2 rounded-full bg-gradient-to-bl from-[#C167F6] to-[#5548C7] text-white px-8 py-1 font-normal text-sm"
